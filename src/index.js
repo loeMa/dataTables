a80e1bdea2d97848f1ac750435401d3ed329fddc
loeMa <chloe.choquet@gmail.com>
2022-12-12 14:30:23 +0100

    commit for npm

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import DataTable from './lib/DataTable';
-import { exampleLabels } from "./data/data";
-import exampleData from './data/MOCK_DATA.json'
 import {StoreProvider} from './lib/utils/storeContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StoreProvider labels={exampleLabels} data={exampleData}>
-      <DataTable labels={exampleLabels} data={exampleData}/>
+    <StoreProvider >
+      <DataTable />
     </StoreProvider>
   </React.StrictMode>
 );
